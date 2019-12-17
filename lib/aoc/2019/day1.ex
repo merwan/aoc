911a9eb6487e43e40a04004e25627cb180a38374
Merouane Atig <merwan@patem.net>
2019-12-17 23:27:42 +0100

    Add dedicated function to calculate fuel requirement

--- lib/aoc/2019/day1.ex
+++ lib/aoc/2019/day1.ex
@@ -2,9 +2,9 @@ defmodule Aoc201901 do
   def run do
     Aoc.Reader.lines(2019, 1)
     |> Stream.map(&String.to_integer/1)
-    |> Stream.map(fn x ->
-      Integer.floor_div(x, 3) - 2
-    end)
+    |> Stream.map(&required_fuel/1)
     |> Enum.sum()
   end
+
+  defp required_fuel(mass), do: Integer.floor_div(mass, 3) - 2
 end
