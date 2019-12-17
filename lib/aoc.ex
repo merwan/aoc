4199be5610b4807ac4f06711ead0ad71f9aa02a8
Merouane Atig <merwan@patem.net>
2019-12-17 22:17:07 +0100

    Naive implementation using stream

--- lib/aoc.ex
+++ lib/aoc.ex
@@ -15,4 +15,14 @@ defmodule Aoc do
   def hello do
     :world
   end
+
+  def day1 do
+    File.stream!("priv/2019/day01.in")
+    |> Stream.map(&(String.trim(&1)))
+    |> Stream.map(&(String.to_integer(&1)))
+    |> Stream.map(fn x -> 
+      Integer.floor_div(x, 3) - 2
+    end)
+    |> Enum.sum
+  end
 end
