a3a7f19b6238ab1b44241dd29ad8b63327413d81
Merouane Atig <merwan@patem.net>
2019-12-17 22:57:20 +0100

    Simplify function call

--- lib/aoc.ex
+++ lib/aoc.ex
@@ -1,24 +1,7 @@
 defmodule Aoc do
-  @moduledoc """
-  Documentation for Aoc.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Aoc.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
-
   def day1 do
     File.stream!("priv/2019/day01.in")
-    |> Stream.map(&(String.trim(&1)))
+    |> Stream.map(&String.trim/1)
     |> Stream.map(&(String.to_integer(&1)))
     |> Stream.map(fn x -> 
       Integer.floor_div(x, 3) - 2
